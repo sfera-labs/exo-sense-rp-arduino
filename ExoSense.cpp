694e9b13ea6f3240a588039301dba20237548811
Giampiero <giampiero.baggiani@gmail.com>
2022-03-14 19:42:15 +0100

    EXOS_RS485.end() after initialization in setup()

--- ExoSense.cpp
+++ ExoSense.cpp
@@ -40,7 +40,8 @@ void ExoSenseClass::setup() {
 
   EXOS_RS485.setRX(EXOS_PIN_RS485_RX);
   EXOS_RS485.setTX(EXOS_PIN_RS485_TX);
-  EXOS_RS485.begin(9600, SERIAL_8N1);
+  EXOS_RS485.begin(9600);
+  EXOS_RS485.end();
 
   Wire.setSDA(EXOS_PIN_I2C_SDA);
   Wire.setSCL(EXOS_PIN_I2C_SCL);
